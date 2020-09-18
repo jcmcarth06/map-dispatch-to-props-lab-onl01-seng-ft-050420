a3455d3ec8779f4f713fe32898aeb1fbd1221cff
Joshua McArthur <jcmcarth06@gmail.com>
2020-09-18 22:41:15 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,10 +23,11 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
+    // debugger;
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
@@ -48,7 +49,6 @@ export class RestaurantInput extends Component {
     );
   }
 };
-
 
 //connect this component by wrapping RestaurantInput below
 export default connect(state => state, { addRestaurant })(RestaurantInput)
